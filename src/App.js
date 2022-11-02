a2ff1b4781003dd27647f7b8425f7447bb06bd2d
nwaamadioha <udokwu.temple960@gmail.com>
2022-11-02 18:26:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { useState, useEffect} from 'react'
 
 function App() {
-  return (
-    <div >
+  const [userInput, setUserInput] = useState('')
+  const [result, setResult] = useState(0)
+  const [num1] = useState(4)
+  const [num2] = useState(5)
 
-    </div>
+  const sum = () => num1 + num2
+
+  useEffect(() => {
+    console.log(`New sum value: ${sum()}`)
+  }, [sum])
+  return (
+    <main className='App'>
+      <input type='text' placeholder='input' value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+
+    </main>
   );
 }
 
